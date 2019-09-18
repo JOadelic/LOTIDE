9421487f57186939bdf6e69e46c3b810c643ac4a
Your Name <you@example.com>
2019-09-18 13:11:56 +0000

    fixed return statement

---
+++ without.js
@@ -0,0 +1,15 @@
+const without = function(source, itemsToRemove) {
+  let newArray = [];
+  
+  for (let i = 0; i < source.length; i++) {
+    if (itemsToRemove.indexOf(source[i]) === -1) {
+      newArray.push(source[i]);
+      
+    } 
+  } 
+  return newArray
+}
+
+
+console.log(without([1, 2, 3], [1])) // => [2, 3]
+console.log(without(["1", "2", "3"], [1, 2, "3"])) // => ["1", "2"]
