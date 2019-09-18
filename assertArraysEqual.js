ab0254d0eaeac66c07cd6f2f2eef69c9878cada5
Your Name <you@example.com>
2019-09-18 00:17:26 +0000

    completed

---
+++ assertArraysEqual.js
@@ -0,0 +1,38 @@
+let eqArrays = function(array1, array2) {
+  // console.log(array1.join());
+  if (array1.length !== array2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+
+let assertArraysEqual = function(a1, a2) {
+  const a = eqArrays(a1, a2);
+  if (a === true) {
+    console.log(`✅ ${a1} === ${a2}`);
+  } else {
+    console.log(`❌ ${a1} !== ${a2}`);
+  }
+};
+
+assertArraysEqual([1, 3, 3], [1, 2, 3]);
+
+
+
+/*
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+*/
