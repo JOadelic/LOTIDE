2b3025073e7f5f00b3e3d10b20a6715476397263
Your Name <you@example.com>
2019-09-18 12:42:23 +0000

    completed

--- middle.js
+++ middle.js
@@ -1,27 +1,3 @@
-/*
-let eqArrays = function(array1, array2) {
-  // console.log(array1.join());
-  if (array1.length !== array2.length) {
-    return false;
-  }
-
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-let assertArraysEqual = function(a1, a2) {
-  const a = eqArrays(a1, a2);
-  if (a === true) {
-    console.log(`✅ ${a1} === ${a2}`);
-  } else {
-    console.log(`❌ ${a1} !== ${a2}`);
-  }
-};
-*/
 const middle = function(array) {
   let center = [];
   let length = array.length;
